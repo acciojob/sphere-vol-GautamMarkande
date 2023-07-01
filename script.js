9720c699670a808f6c7a8e39b02035be4adae081
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-01 17:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function volume_sphere(e) {
 	e.preventDefault();
 	let rad = document.getElementById("radius").value;
 	let ans = (4/3)*Math.PI*(rad*rad*rad);
-	let vol = document.getElementById("volume").value=ans;
+	let vol = document.getElementById("volume").value=ans.toFixed(4);
   
 } 
 
